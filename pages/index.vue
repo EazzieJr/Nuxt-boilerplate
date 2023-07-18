84a9e55a54a488428e43ef85661fc8442550124c
Ganiyu idris <golamide27@gmail.com>
2023-07-18 22:52:45 +0100

    Setup Lenis scroll

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,30 @@
 </template>
 
 <script>
+import Lenis from '@studio-freight/lenis'
+
 export default {
   name: 'IndexPage',
+
+  methods: {
+    initLenis() {
+      const lenis = new Lenis({duration: 2})
+
+      lenis.on('scroll', (e) => {
+        // console.log(e)
+      })
+
+      function raf(time) {
+        lenis.raf(time)
+        requestAnimationFrame(raf)
+      }
+
+      requestAnimationFrame(raf)
+    }
+  }, 
+
+  mounted() {
+    this.initLenis()
+  }
 }
 </script>
