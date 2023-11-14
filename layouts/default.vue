afd62564c101d69dc60c3c92986472175e04bb04
Ganiyu idris <golamide27@gmail.com>
2023-11-14 08:05:01 +0100

    Add Lenis scroll animation to default layout and update index page

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,26 @@
 
 <script>
 export default {
-	
+	methods: {
+    initLenis() {
+      const lenis = new Lenis({duration: 2})
+
+      lenis.on('scroll', (e) => {
+        // console.log(e)
+      })
+
+      function raf(time) {
+        lenis.raf(time)
+        requestAnimationFrame(raf)
+      }
+
+      requestAnimationFrame(raf)
+    }
+  }, 
+
+  mounted() {
+    this.initLenis()
+  }
 }
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <section class="bg-black h-screen w-4/5">
+  <section class="bg-black h-screen text-white text-8xl center">
+    <h1>
+      Yet another Nuxt.js + TailwindCSS + Lenis template
+    </h1>
   </section>
 </template>
 
@@ -8,26 +11,5 @@ import Lenis from '@studio-freight/lenis'
 
 export default {
   name: 'IndexPage',
-
-  methods: {
-    initLenis() {
-      const lenis = new Lenis({duration: 2})
-
-      lenis.on('scroll', (e) => {
-        // console.log(e)
-      })
-
-      function raf(time) {
-        lenis.raf(time)
-        requestAnimationFrame(raf)
-      }
-
-      requestAnimationFrame(raf)
-    }
-  }, 
-
-  mounted() {
-    this.initLenis()
-  }
 }
 </script>
